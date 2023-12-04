9e4724740c43c040cd28e16eca9d3f93cf680240
Brandon Lorenz <blorenz@gmail.com>
2023-12-04 11:09:33 -0500

    Integrate flatRoutes function into plugin configuration
    The configuration in `vite.config.ts` has been updated to utilize the flatRoutes function from `remix-flat-routes/esm/index.js`. This change will aid in defining routes more efficiently by supporting nested route structures, represented with a hyphen ("-").

--- vite-express-tailwind/vite.config.ts
+++ vite-express-tailwind/vite.config.ts
@@ -1,7 +1,17 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { flatRoutes } from "./remix-flat-routes/esm/index.js";
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix({
+        routes: async (defineRoutes) => {
+      return flatRoutes(`routes`, defineRoutes, {
+        nestedFolderChar: `-`,
+      })
+    }
+
+
+
+  }), tsconfigPaths()],
 });
